13d9e20e6848472bdc5cabb71a4149295f7ef084
niederle <claude.niederlender@inist.fr>
2019-01-28 17:31:12 +0100

    passage à mapLimit, et ajout d'un suffixe d'URL

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # harvest-crossref
+
+Cet outil permet d'automatiser l'interrogation à l'API Crossref pour récupérer un jeu de notices XML à partir d'une liste de DOI. 
+Les appels à l'API Crossref se font via l'URL `https://api.crossref.org/works/<doi>.xml`
+
+Usage : 
+
+`./crawl-by-dois.js <file> <output_dir> <suffix_url>`
+
+avec :
+
+* `file` Un fichier texte contenant un DOI par ligne
+* `output_dir` le répertoire de destination, dans lequel seront écrites les notices XML
+* `suffix_url` un suffixe systématiquement ajouté à la fin de l'URL d'appel à l'API Crossref. Par exemple : `?usr=...&pwd=...` si vous êtes abonnés à Metadata Plus
--- crawl-by-dois.js
+++ crawl-by-dois.js
@@ -6,8 +6,10 @@ const path = require('path');
 const dois = fs.readFileSync(process.argv[2],{encoding:'utf8'}).trim().split("\n");
 const tmpDir = '/dev/shm';
 const outputDir = process.argv[3];
+const suffixUrl = process.argv[4];
 const crossrefPrefix = 'https://api.crossref.org/works/';
-const eachOfLimit =require('async/eachOfLimit');
+// const eachOfLimit =require('async/eachOfLimit');
+const mapLimit =require('async/mapLimit');
 const spawnSync = require('child_process').spawnSync;
 
 // dois.forEach(myDoi => {
@@ -27,9 +29,20 @@ const spawnSync = require('child_process').spawnSync;
 // });
 
 
-eachOfLimit(dois, 5, function(myDoi,doiIndex,cb) {
+mapLimit(dois, 10, function(myDoi,cb) {
     let xmlData = '';
-    request.get(crossrefPrefix+myDoi+'.xml')
+    let statusCode = 0;
+    const suffix = (suffixUrl && suffixUrl!=='') ? suffixUrl : '';
+    const apiDocUrl = crossrefPrefix+myDoi+'.xml'+suffix;
+    request.get(apiDocUrl)
+    .on('response', function(response) {
+        if (response.statusCode !== 200 ) {
+            console.error("Erreur dans la récupération de "+apiDocUrl);
+            console.error("Status code : "+response.statusCode);
+            console.error("Body renvoyé : "+xmlData);
+        }
+        statusCode = response.statusCode;
+    })
     .on('error', function(err) {
         console.log(err)
         cb(err);
@@ -39,20 +52,31 @@ eachOfLimit(dois, 5, function(myDoi,doiIndex,cb) {
         // console.log(new Buffer(data).toString('utf8')) // 200    
     })
     .on('end', function(response) {
-        const tmpPath = path.join(tmpDir,myDoi.replace(/\//g,'_')+'.xml');
-        const outputPath = path.join(outputDir,myDoi.replace(/\//g,'_')+'.xml');
-        // console.log('xml string : ',xmlString)
-        console.log('écriture du fichier '+outputPath);
-        fs.writeFileSync(tmpPath,xmlData.toString('utf8'),{encoding:'utf8'});
-        const args = ['--output',outputPath ,'crossref.xsl',tmpPath];
-        const opts = {}
-        const res = spawnSync('xsltproc',args,opts);
-        // fs.unlinkSync(tmpPath);
-        cb();
+        if (statusCode !== 200 ) {
+            cb();
+        } else {
+            const tmpPath = path.join(tmpDir,myDoi.replace(/\//g,'_')+'.xml');
+            const outputPath = path.join(outputDir,myDoi.replace(/\//g,'_')+'.xml');
+            // console.log('xml string : ',xmlString)
+            console.log('écriture du fichier '+outputPath);
+
+            
+            fs.writeFileSync(tmpPath,xmlData.toString('utf8'),{encoding:'utf8'});
+            const args = ['--output',outputPath ,'crossref.xsl',tmpPath];
+            const opts = {}
+            const res = spawnSync('xsltproc',args,opts);
+            if (res.status === 0) {
+                console.log(outputPath+" : génération OK");
+            } else {
+                console.error(outputPath+" : fichier non généré (code "+res.status+").");
+                console.error("Erreur : "+res.stderr);
+            }
+            fs.unlinkSync(tmpPath);
+            cb(res.status);
+        }
     });
 
-    }, function(){
-        console.log("fini !")
-    }
-);
+}, function(){
+    console.log("fini !")
+});
 
